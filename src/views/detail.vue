e82caaff62f0e6170cda24714e2a788ea4672939
Wang Jing <212598210@qq.com>
2019-11-06 21:03:47 +0800

    first

---
+++ src/views/detail.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        详情页面
+        <h2>{{info.title}}</h2>
+        <p>{{info.time}}</p>
+        <img :src="info.banner" alt="">
+        <p>{{`${info.favor} 赞同 ${info.comment} 评论`}} </p>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            info:{}
+        }
+    },
+    async created(){
+        let id=this.$route.params.id;
+        let data=await axios.get(`/api/detail?id=${id}`)
+        this.info=data.data;
+    }
+}
+</script>
